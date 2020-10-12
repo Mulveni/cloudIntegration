c07e63a35891aa4382be47c49ad92b02ad611cc8
Mulveni <46740528+Mulveni@users.noreply.github.com>
2020-10-12 11:49:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express')
 const { v4: uuidv4 } = require('uuid');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 //query param == polku/i?-----
 
